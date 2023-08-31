711c05f5571c63203af7347413c1c2f7f783c978
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2023-08-31 14:04:58 +0900

    SEO周り

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -52,9 +52,7 @@ export const Layout = ({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+            gtag('config', '${GA_TRACKING_ID}');
         `,
               }}
             />
